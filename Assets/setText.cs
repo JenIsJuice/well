f3d908273c7847cbdf8fff39b75b906dcd14f017
JenIsJuice <juice_841201@hotmail.com>
2017-02-28 21:52:52 +0800

    Add files via upload

---
+++ Assets/setText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+
+public class setText : MonoBehaviour{
+	public Text txt;
+	public GameObject Cube; //make inspector script has a var space to define
+
+	void Start(){
+		txt.text = "show from here"; //initial perfrom;
+	}
+
+	void Update(){
+		float x = Cube.transform.position.x;//get postion and set in var xyz;
+		float y = Cube.transform.position.y;	
+		float z = Cube.transform.position.z;
+
+		string _x = x.ToString(); //change float to string var type
+		string _y = y.ToString();
+		string _z = z.ToString();
+
+		txt.text = ("("+ _x + "," + _y + "," + _z + ")");
+		Debug.Log (txt.text);
+	}
+}
